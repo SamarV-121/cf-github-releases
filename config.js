dfb5dd77efb768c5bc962caf2752671e8699aad2
SamarV-121 <samarvispute121@pm.me>
2022-05-30 15:20:59 +0530

    Setup github actions
    Change-Id: I4dc6808f99f9513cf1148260ae58cb825b31fb63

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 const DOMAIN = undefined;
 
 // The repository in which you're providing download files
-const REPOSITORY = "octocat/hello-world";
+const REPOSITORY = "SamarV-121/releases";
 
 // The default tag if not specified
 const DEFAULT_TAG = "default";
 
 // Give your site a lovely name
-const SITE_NAME = "My GitHub bucket";
+const SITE_NAME = "CI Builds";
 
 // Any extra <head> content you want. CSS, favicons are all welcome!
 const HEAD = ``;
